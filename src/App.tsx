22758927884c296ce1265f8ea18826b270f7c3e1
fadeyush <a.fadeyush@gmail.com>
2024-01-15 18:06:12 +0300

    создание списка пользователей

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card, { CardVariant } from './components/Card';
+import UserList from './components/UserList';
+import { IUser } from './types/types';
 
 const App = () => {
+
+  const users: IUser[] = [{
+    id: 1,
+    name: "Leanne Graham",
+    email: "Sincere@april.biz",
+    adress: {
+      street: "Kulas Light",
+      city: "Gwenborough",
+      zipcode: "92998-3874"
+    }},
+    {
+      id: 2,
+      name: "Leanne Graham",
+      email: "Sincere@april.biz",
+      adress: {
+        street: "Kulas Light",
+        city: "Gwenborough",
+        zipcode: "92998-3874"
+      }}]
+
   return (
     <div>
       <Card onClick={(num: number) => console.log(`click ${num}`)} variant={CardVariant.outlined} width='200px' height='200px'>
         <button>Кнопка</button>
       </Card>
+      <UserList users={users}></UserList>
     </div>
   );
 };
