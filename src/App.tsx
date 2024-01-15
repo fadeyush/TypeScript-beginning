a0be7b8fe0112f81a28a4eaa6b11472fff8f4466
fadeyush <a.fadeyush@gmail.com>
2024-01-15 20:24:02 +0300

    универсальный компонент List

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import Card, { CardVariant } from './components/Card';
-import UserList from './components/UserList';
 import { IUser } from './types/types';
 import axios from 'axios';
+import List from './components/List';
+import UserItem from './components/UserItem';
 
 const App = () => {
 
@@ -14,7 +15,6 @@ const App = () => {
 
   async function fetchUsers() {
     const response = await axios.get<IUser[]>(`https://jsonplaceholder.typicode.com/users`);
-    console.log(response)
     setUsers(response.data);
   }
 
@@ -23,7 +23,7 @@ const App = () => {
       <Card onClick={(num: number) => console.log(`click ${num}`)} variant={CardVariant.outlined} width='200px' height='200px'>
         <button>Кнопка</button>
       </Card>
-      <UserList users={users}></UserList>
+      <List items={users} renderItem={(user: IUser) => <UserItem key={user.id} user={user}/>}/>
     </div>
   );
 };
