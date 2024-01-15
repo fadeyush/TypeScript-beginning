ee242c9da54fda4a1671f0f2f955193b8ee6aa53
fadeyush <a.fadeyush@gmail.com>
2024-01-15 18:31:56 +0300

    получение списка пользователей axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Card, { CardVariant } from './components/Card';
 import UserList from './components/UserList';
 import { IUser } from './types/types';
+import axios from 'axios';
 
 const App = () => {
 
-  const users: IUser[] = [{
-    id: 1,
-    name: "Leanne Graham",
-    email: "Sincere@april.biz",
-    adress: {
-      street: "Kulas Light",
-      city: "Gwenborough",
-      zipcode: "92998-3874"
-    }},
-    {
-      id: 2,
-      name: "Leanne Graham",
-      email: "Sincere@april.biz",
-      adress: {
-        street: "Kulas Light",
-        city: "Gwenborough",
-        zipcode: "92998-3874"
-      }}]
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, [])
+
+  async function fetchUsers() {
+    const response = await axios.get<IUser[]>(`https://jsonplaceholder.typicode.com/users`);
+    console.log(response)
+    setUsers(response.data);
+  }
 
   return (
     <div>
