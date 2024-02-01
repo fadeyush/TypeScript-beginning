f9bab522790681efe3dc2b9cbd6de774530d0362
fadeyush <a.fadeyush@gmail.com>
2024-02-01 17:53:49 +0300

    получение и отображение данных по отдельному пользователю

--- src/pages/UserItemPage.tsx
+++ src/pages/UserItemPage.tsx
@@ -1,9 +1,36 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { IUser } from '../types/types';
+import axios from 'axios';
+
+type UserItemPageParams = {
+    id: string;
+};
 
 const UserItemPage: FC = () => {
+    const pageNumber = useParams<UserItemPageParams>();
+    const [user, setUser] = useState<IUser | null>(null);
+    
+    useEffect(() => {
+        fetchUser();
+      }, [])
+
+    async function fetchUser() {
+        try {
+            const response = await axios.get<IUser>(`https://jsonplaceholder.typicode.com/users/${pageNumber.id}`);
+            setUser(response.data);
+            console.log(response.data)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+
     return (
         <div>
-            Страница пользователя
+            <h1>Страница пользователя {user?.name}</h1>
+            <p>email пользователя: {user?.email}</p>
+            <p>адрес пользователя: {user?.address.city} {user?.address.street}</p>
         </div>
     );
 };
